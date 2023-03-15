1bcff27759aaa22212ae8433bb72e4faa353930d
TheDanishMexican <danieljchristjansen@gmail.com>
2023-03-15 12:46:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ function initApp() {
 
 // modeSelected called when #select-color-mode changes value (the user select color mode)
 function modeSelected() {
-  console.log("New color mode selected");
   const selectedColorMode = this.value;
   changeMode(selectedColorMode);
   saveUserColorMode(selectedColorMode);
@@ -22,13 +21,10 @@ function changeMode(mode) {
   resetColorMode();
   if (mode === "dark") {
     document.body.classList.add("dark-mode");
-    console.log("This is dark mode");
   } else if (mode === "yellow") {
     document.body.classList.add("yellow-mode");
-    console.log("This is yellow mode");
   } else if (mode === "blue") {
     document.body.classList.add("blue-mode");
-    console.log("This is blue mode");
   }
 }
 function resetColorMode() {
@@ -45,4 +41,11 @@ function readUserColorMode() {
   return userColorMode;
 }
 
-function detectUserPreference() {}
+function detectUserPreference() {
+  const modeFromLocalStorage = readUserColorMode();
+
+  if (modeFromLocalStorage) {
+    changeMode(modeFromLocalStorage);
+    document.querySelector("#select-color-mode").value = modeFromLocalStorage;
+  }
+}
